371f4312969e95e330d6bfea47bb3a143b2eb946
Ans de Nijs <ansdenijs@gmail.com>
2019-05-23 10:14:21 +0200

    check on solvability

---
+++ src/util/checkSolvability.js
@@ -0,0 +1,22 @@
+export default function isSolvable(tiles) {
+  const inversionCount = getInvCount(tiles);
+  const positionBlankTile = findXPosition(tiles);
+  const rowPositionBlankTile = Math.floor(positionBlankTile / 4);
+  if (rowPositionBlankTile % 2 == 1) return inversionCount % 2 == 1;
+  else return inversionCount % 2 == 0;
+}
+
+function findXPosition(tiles) {
+  const blankTile = tiles.filter(tile => tile.position === 0);
+  return blankTile.styles.order;
+}
+
+function getInvCount(tiles) {
+  let invertCount = 0;
+  for (let i = 0; i < 4 * 4 - 1; i++) {
+    if (tiles[i].styles.order > tiles[i + 1].styles.order) {
+      invertCount++;
+    }
+  }
+  return invertCount;
+}
